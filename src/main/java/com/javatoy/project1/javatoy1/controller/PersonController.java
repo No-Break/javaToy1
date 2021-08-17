e8f15ec2d6a2d29c3768ea84f72a1d1ed6119681
wonho choi <30523167+choiwonho@users.noreply.github.com>
2021-08-18 05:15:05 +0900

    Study final feature (#11)
    Parameter validator feature and Pageable feature

--- src/main/java/com/javatoy/project1/javatoy1/controller/PersonController.java
+++ src/main/java/com/javatoy/project1/javatoy1/controller/PersonController.java
@@ -9,9 +9,15 @@ import com.javatoy.project1.javatoy1.repository.PersonRepository;
 import com.javatoy.project1.javatoy1.service.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RequestMapping(value = "/api/person")
 @RestController
@@ -20,6 +26,11 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @GetMapping
+    public Page<Person> getAll(@PageableDefault Pageable pageable) {
+        return personService.getAll(pageable);
+    }
+
     @GetMapping("/{id}")
     public Person getPerson(@PathVariable Long id) {
         return personService.getPerson(id);
@@ -27,7 +38,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void postPerson(@RequestBody PersonDto personDto) {
+    public void postPerson(@RequestBody @Valid PersonDto personDto) {
         personService.put(personDto);
     }
 
